d492e38de43506ea82bd4aa5edcc8e7f79f927a6
Alexey <danilov.all@yandex.ru>
2020-09-18 11:43:25 +0300

    Circle change

--- src/ru/mirea/practice3/Circle.java
+++ src/ru/mirea/practice3/Circle.java
@@ -13,11 +13,6 @@ public class Circle {
         calcLenght();
     }
 
-    public Circle(int radius) {
-        this.radius = radius;
-        calcLenght();
-    }
-
     public int getRadius() {
         return radius;
     }
